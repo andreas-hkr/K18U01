ac48979a91cca33ffa94b51de85f52521c37098d
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-21 13:38:39 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Läs in ett godtyckligt antal tal och lagra dem i en lista (avsluta inläsning med EoF)
+        // Loopa igenom listan och skriv ut alla tal, men skriv inte ut dubbletter
+
+
+    }
+}
